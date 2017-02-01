ad61fc1c8141045b778f5468ab9ec269de24fff7
David van Laatum <david@vanlaatum.id.au>
2017-02-01 19:55:01 +1030

    #21 Session mode broken over tcp integration test

--- install_clam.sh
+++ install_clam.sh
@@ -2,5 +2,7 @@
 apt-get update -qq
 apt-get install -qq clamav-daemon
 echo TCPSocket 3310 >> /etc/clamav/clamd.conf
+echo User travis >> /etc/clamav/clamd.conf
+cat /etc/clamav/clamd.conf
 freshclam
 /etc/init.d/clamav-daemon start
